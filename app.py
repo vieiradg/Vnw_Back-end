c3b2fd9c0bb84bd7dd872108297b3aeec434e760
vieiradg <diegorusty40@gmail.com>
2025-05-07 23:51:55 -0300

    update
    add

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 # RESPONSÁVEL APENAS PELA EXECUÇÃO DO SERVIDOR
 
 from src.app import create_app
+import os
 
 app = create_app()
-app.run(debug=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Só roda isso localmente
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
